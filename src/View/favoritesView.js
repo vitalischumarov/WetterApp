ab7e5bb292228a3a2304e7c517cf6afc14b68917
Vitali Schumarov <v.schumarov@icloud.com>
2025-02-26 19:18:40 +0100

    second page

--- src/View/favoritesView.js
+++ src/View/favoritesView.js
@@ -1,5 +1,33 @@
 import "../Style/favoritesViewStyle.scss";
 
+const app = document.querySelector(".app");
+
 export function display() {
-  document.querySelector(".app").innerHTML = "helloo";
+  showHeader();
+  changeBackground();
+}
+
+function showHeader() {
+  const basicStructure = `
+    <div class="app">
+          <div class="header_favorite">
+              <span class="header_favorite__text">Wetter</span>
+              <button class="header_favorite__button">Bearbeiten</button>
+          </div>
+          <div class="inputField">
+              <input
+                  type="text"
+                  class="inputField__element"
+                  placeholder="Gebe den Namen einer Stadt an"
+              />
+          </div>
+          <div class="favoriteList"></div>
+      </div>
+    `;
+  app.innerHTML = basicStructure;
+}
+
+function changeBackground() {
+  app.style.backgroundImage = "none";
+  app.style.backgroundColor = "red";
 }
