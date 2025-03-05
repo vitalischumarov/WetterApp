2f22dedaabbdea208a55d48574dd072b38a7460d
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-05 18:14:29 +0100

    https inserted

--- src/Model/api.js
+++ src/Model/api.js
@@ -2,7 +2,7 @@ import "../View/loadingView";
 
 export async function fetchData(cityName) {
   const WEATHER_API_1 =
-    "http://api.weatherapi.com/v1/forecast.json?key=4d9509708acc49a6a8740155253101&q=id:";
+    "https://api.weatherapi.com/v1/forecast.json?key=4d9509708acc49a6a8740155253101&q=id:";
   const WEATHER_API_2 = "&lang=DE&days=3";
   let response = await fetch(WEATHER_API_1 + cityName + WEATHER_API_2);
   if (!response.status) {
