a1c822207d11bcf61a7f752b91759e43b95998bb
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-05 20:53:38 +0100

    url for pictures changed

--- src/Model/condition.js
+++ src/Model/condition.js
@@ -3,7 +3,10 @@ export function getConditionImagePath(code, isNight = false) {
 
   if (!condition) return null;
 
-  return "./public/conditionImages/" + condition[isNight ? "night" : "day"];
+  return (
+    "https://vitalischumarov.github.io/WetterApp/conditionImages/" +
+    condition[isNight ? "night" : "day"]
+  );
 }
 
 const CONDITION_IMAGES = [
