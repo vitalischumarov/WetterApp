c71ef9276468a1a11e6350f2183ca97eeb039e09
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-05 20:44:54 +0100

    condition changed

--- src/Model/condition.js
+++ src/Model/condition.js
@@ -3,7 +3,7 @@ export function getConditionImagePath(code, isNight = false) {
 
   if (!condition) return null;
 
-  return "./src/conditionImages/" + condition[isNight ? "night" : "day"];
+  return "./public/conditionImages/" + condition[isNight ? "night" : "day"];
 }
 
 const CONDITION_IMAGES = [
